3f250df3ae28cab882e9431b08a07bee60d3f031
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2023-12-11 14:48:55 +0700

    Теперь точно сделал 3 шаг

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { whatYourName } from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!")
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`)
+console.log("Welcome to the Brain Games!");
+const playerName = whatYourName();
+console.log(`Hello, ${playerName}!`);
