52488f54d368bf2ac9b77026841a59900c6d5031
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2023-12-11 15:50:27 +0700

    Чуток поправил всё перед пятым шагом

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import whatYourName from '../src/cli.js';
+import helloUnknownName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const playerName = whatYourName();
-console.log(`Hello, ${playerName}!`);
+helloUnknownName();
