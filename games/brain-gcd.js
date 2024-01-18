67be06f51c3e67eb70fde55f80c7149879f25b6c
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2024-01-18 20:13:46 +0700

    После завершения логики раундов в отдельном файле куда бодрее идёт, конечно

---
+++ games/brain-gcd.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import helloUnknownName from '../src/cli.js';
+import threeRoundsGame from '../src/index.js';
+
+const playerName = helloUnknownName();
+console.log('Find the greatest common divisor of given numbers.');
+
+const brotherhoodNOD = () => {
+  const randomNumberFromZeroToFifty = 49;
+  const antiZero = 1;
+  const randomNumberOne = Math.floor(Math.random() * randomNumberFromZeroToFifty + antiZero);
+  const randomNumberTwo = Math.floor(Math.random() * randomNumberFromZeroToFifty + antiZero);
+  let theSmallerOfTwoNumbers;
+  let resultForGame;
+
+  if (randomNumberOne < randomNumberTwo) {
+    theSmallerOfTwoNumbers = randomNumberOne;
+  } else {
+    theSmallerOfTwoNumbers = randomNumberTwo;
+  }
+  const searchGCDCorrectAnswer = () => {
+    for (let i = theSmallerOfTwoNumbers; i >= 1; i -= 1) {
+      if (randomNumberOne % i === 0 && randomNumberTwo % i === 0) {
+        return i;
+      }
+    }
+    return 1;
+  };
+
+  const correctAnswerToTheQuestion = searchGCDCorrectAnswer();
+
+  console.log(`Question: ${randomNumberOne} ${randomNumberTwo}`);
+  // Правильный ответ. console.log(correctAnswerToTheQuestion);
+  const playerAnswer = readlineSync.question('Your answer: ');
+  if (playerAnswer !== correctAnswerToTheQuestion.toString()) {
+    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswerToTheQuestion}'.\nLet's try again, ${playerName}!`);
+    resultForGame = 'Defeat';
+    return resultForGame;
+  }
+  resultForGame = 'Win';
+  console.log('Correct!');
+  return resultForGame;
+};
+
+threeRoundsGame(brotherhoodNOD, playerName);
