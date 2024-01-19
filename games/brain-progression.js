4257deec5eda2f84a939445cc1f590b3ea4015e2
ThisisHappyEL <1379This_is_HappyEL@mail.ru>
2024-01-19 16:41:27 +0700

    Ну теперь то нашёл ошибку. Ведь так?

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -10,7 +10,7 @@ const arithmeticProgression = () => {
   console.log('What number is missing in the progression?');
   const randomNumberFromZeroToFifty = 51;
   const randomNumberFromOneToTen = 9;
-  const randomNumberFromZeroToTen = 11;
+  const randomNumberFromZeroToTen = 10;
   const antiZero = 1;
   const randomStartsNum = Math.floor(Math.random() * randomNumberFromZeroToFifty);
   const randomStepUp = Math.floor(Math.random() * randomNumberFromOneToTen + antiZero);
